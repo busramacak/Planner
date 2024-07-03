86af014247549ff4eb24ca4d88bde4942945f32f
Busra <busramacak@outlook.com>
2024-07-03 17:06:33 +0300

    Enable image sharing for notes with title and content integration
    - Integrated title and content into designed layout for image sharing
    - Converted layout to bitmap and shared as PNG

--- app/src/main/java/com/bmprj/planner/ui/addNote/AddNoteFragment.kt
+++ app/src/main/java/com/bmprj/planner/ui/addNote/AddNoteFragment.kt
@@ -3,13 +3,19 @@ package com.bmprj.planner.ui.addNote
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.FileProvider
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -17,6 +23,7 @@ import com.bmprj.planner.R
 import com.bmprj.planner.base.BaseFragment
 import com.bmprj.planner.databinding.FragmentAddNoteBinding
 import com.bmprj.planner.databinding.ShareBottomLayoutBinding
+import com.bmprj.planner.databinding.ShareImageLayoutBinding
 import com.bmprj.planner.model.Note
 import com.bmprj.planner.utils.clearFocus
 import com.bmprj.planner.utils.getDateTime
@@ -29,6 +36,9 @@ import com.bmprj.planner.utils.setVisibility
 import com.bmprj.planner.utils.toast
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 import java.util.Stack
 
 
@@ -39,7 +49,8 @@ class AddNoteFragment : BaseFragment<FragmentAddNoteBinding>(R.layout.fragment_a
     private val bundle :AddNoteFragmentArgs by navArgs()
     private val noteId:Int by lazy { bundle.id }
     private lateinit var oldNote:Note
-    @SuppressLint("ClickableViewAccessibility")
+    private lateinit var imageViewResult: ImageView
+    @SuppressLint("ClickableViewAccessibility") 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun initView(view: View) {
 
@@ -79,8 +90,69 @@ class AddNoteFragment : BaseFragment<FragmentAddNoteBinding>(R.layout.fragment_a
         bottomSheetDialog.setContentView(bottomSheetBinding.root)
 
         bottomSheetBinding.btnShareText.setOnClickListener { shareNoteAsText(binding.content.text.toString()) }
+        bottomSheetBinding.btnShareImage.setOnClickListener { shareNoteasImage(binding.title.text.toString(),binding.content.text.toString()) }
         bottomSheetBinding.closeButton.setOnClickListener { bottomSheetDialog.dismiss() }
         bottomSheetDialog.show()
+    }
+
+    @SuppressLint("Range")
+    private fun shareNoteasImage(title:String, content: String) {
+
+        val imageLayoutBinding = ShareImageLayoutBinding.inflate(LayoutInflater.from(requireContext()))
+
+        imageLayoutBinding.title.text=title
+        imageLayoutBinding.content.text=content
+
+        val width = 800  // Set your desired width
+        val height = ViewGroup.LayoutParams.WRAP_CONTENT
+        imageLayoutBinding.root.layoutParams=ViewGroup.LayoutParams(width,height)
+
+        val specWidth = View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY)
+        val specHeight = View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.UNSPECIFIED)
+        imageLayoutBinding.root.measure(specWidth, specHeight)
+        imageLayoutBinding.root.layout(0, 0, imageLayoutBinding.root.measuredWidth, imageLayoutBinding.root.measuredHeight)
+
+        // Create a Bitmap with the same dimensions as the view
+        val bitmap = Bitmap.createBitmap(imageLayoutBinding.root.measuredWidth, imageLayoutBinding.root.measuredHeight, Bitmap.Config.ARGB_8888)
+
+        // Create a Canvas and associate it with the Bitmap
+        val canvas = Canvas(bitmap)
+
+        // Set background color if necessary
+        canvas.drawColor(Color.WHITE)
+
+        // Draw the view on the Canvas
+        imageLayoutBinding.root.draw(canvas)
+
+        // Display the created bitmap in ImageView
+//        imageViewResult.setImageBitmap(bitmap)
+
+        // Save the bitmap as an image file and share
+        saveBitmap(bitmap)
+    }
+
+    private fun saveBitmap(bitmap: Bitmap) {
+        val fileName = "my_image.png"
+        val file = File(requireContext().getExternalFilesDir(null), fileName)
+        try {
+            val stream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+            stream.flush()
+            stream.close()
+            shareImage(file)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun shareImage(file: File) {
+        val uri = FileProvider.getUriForFile(requireContext(), requireContext().packageName + ".provider", file)
+        val shareIntent = Intent(Intent.ACTION_SEND).apply {
+            type = "image/png"
+            putExtra(Intent.EXTRA_STREAM, uri)
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+        startActivity(Intent.createChooser(shareIntent, "Share image"))
     }
 
     private fun shareNoteAsText(text:String) {
