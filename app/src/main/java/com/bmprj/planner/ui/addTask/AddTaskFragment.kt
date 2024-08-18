de4ffeb762d3378e71b766e66ba81757731fe2cf
Busra <busramacak@outlook.com>
2024-08-18 16:52:57 +0300

    Handle notification permission request in AddTaskFragment and add necessary permissions to the manifest.

--- app/src/main/java/com/bmprj/planner/ui/addTask/AddTaskFragment.kt
+++ app/src/main/java/com/bmprj/planner/ui/addTask/AddTaskFragment.kt
@@ -1,8 +1,14 @@
 package com.bmprj.planner.ui.addTask
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Build
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.bmprj.planner.R
@@ -11,23 +17,48 @@ import com.bmprj.planner.databinding.FragmentAddTaskBinding
 import com.bmprj.planner.model.Task
 import com.bmprj.planner.utils.getDatee
 import com.bmprj.planner.utils.setRandomAlarm
+import com.bmprj.planner.utils.toast
 import com.google.android.material.datepicker.MaterialDatePicker
-import com.google.android.material.timepicker.MaterialTimePicker
-import com.google.android.material.timepicker.TimeFormat
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
-class AddTaskFragment : BaseFragment<FragmentAddTaskBinding>(R.layout.fragment_add_task){
+class AddTaskFragment : BaseFragment<FragmentAddTaskBinding>(R.layout.fragment_add_task) {
+
 
     private val addTaskViewModel by viewModels<AddTaskViewModel>()
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                continueWithAction()
+            } else {
+                toast("Bildirim izni verilmedi.")
+            }
+        }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun continueWithAction() {
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select date")
+            .build()
+
+        datePicker.show(parentFragmentManager, "tag")
+
+        datePicker.addOnPositiveButtonClickListener {
+            binding.dateText.text = it.getDatee()
+            setRandomAlarm(it.getDatee())
+        }
+    }
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun initView(view: View) {
 
         initLiveDataObservers()
 
-        with(binding){
-            createTaskButton.setOnClickListener{ saveButtonClick()}
+        with(binding) {
+            createTaskButton.setOnClickListener { saveButtonClick() }
             dateButton.setOnClickListener { dateButtonClick() }
             taskBackButton.setOnClickListener { backButtonClick() }
         }
@@ -40,52 +71,66 @@ class AddTaskFragment : BaseFragment<FragmentAddTaskBinding>(R.layout.fragment_a
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun dateButtonClick() {
-        val datePicker =
-            MaterialDatePicker.Builder.datePicker()
-                .setTitleText("Select date")
-                .build()
 
-        datePicker.show(parentFragmentManager,"tag")
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                continueWithAction()
+            }
 
-        datePicker.addOnPositiveButtonClickListener {
-            binding.dateText.text=it.getDatee()
-            setRandomAlarm(it.getDatee())
+            shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+                showPermissionExplanation()
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
         }
+    }
+
+    private fun showPermissionExplanation() {
+        toast("Bildirim izni verilmemiş ayarlardan bildirimlere izin verin pls.")
     }
 
     private fun saveButtonClick() {
 
-        with(binding){
-            var marketing =false
-            var meeting=false
-            var planning=false
-            var funn=false
-            var other=false
+        with(binding) {
+            var marketing = false
+            var meeting = false
+            var planning = false
+            var funn = false
+            var other = false
 
             val checkedCategoryIds = chipGroup.checkedChipIds
-            for(i in checkedCategoryIds){
-                when(i){
-                    marketingCategory.id ->{
-                        marketing=true
+            for (i in checkedCategoryIds) {
+                when (i) {
+                    marketingCategory.id -> {
+                        marketing = true
                     }
-                    meetingCategory.id ->{
-                        meeting=true
+
+                    meetingCategory.id -> {
+                        meeting = true
                     }
-                    planningCategory.id ->{
-                        planning=true
+
+                    planningCategory.id -> {
+                        planning = true
 
                     }
-                    funCategory.id ->{
-                        funn=true
+
+                    funCategory.id -> {
+                        funn = true
                     }
-                    otherCategory.id ->{
-                        other=true
+
+                    otherCategory.id -> {
+                        other = true
                     }
                 }
             }
 
             val task = Task(
-                title =taskTitleEditText.text.toString(),
+                title = taskTitleEditText.text.toString(),
                 description = descriptionEditText.text.toString(),
                 isChecked = false,
                 taskDate = dateText.text.toString(),
@@ -93,7 +138,8 @@ class AddTaskFragment : BaseFragment<FragmentAddTaskBinding>(R.layout.fragment_a
                 meeting = meeting,
                 planning = planning,
                 funn = funn,
-                other = other)
+                other = other
+            )
 
             addTaskViewModel.insertTask(task)
             val action = AddTaskFragmentDirections.actionAddTaskFragmentToTasksFragment()
--- app/src/main/java/com/bmprj/planner/utils/AlarmReceiver.kt
+++ app/src/main/java/com/bmprj/planner/utils/AlarmReceiver.kt
@@ -1,19 +1,44 @@
 package com.bmprj.planner.utils
 
+import android.Manifest
+import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.media.RingtoneManager
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.bmprj.planner.R
 
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
-        var alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-        if(alarmUri == null){
-            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        }
+      showNotification(p0)
+    }
 
-        val ringtone = RingtoneManager.getRingtone(p0,alarmUri)
-        ringtone.play()
+    private fun showNotification(p0: Context?) {
+
+        val notificationManager = p0?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notificationBuilder = NotificationCompat.Builder(p0, "channel_id")
+            .setSmallIcon(R.drawable.icon_alarm)
+            .setContentTitle("Alarm Notification")
+            .setContentText("This is your alarm notification!")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("channel_id", "Channel Name", NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = "Channel Description"
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(1, notificationBuilder.build())
+
     }
 }
